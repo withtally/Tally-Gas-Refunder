2948dca0857a83b0320ce0218bad06bf4ed7ba86
Aleksey <alexeitcekov@abv.bg>
2021-03-17 16:05:58 +0200

    temp

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -3,11 +3,23 @@
 pragma solidity ^0.7.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "./IRegistry.sol";
 
 contract Registry is Ownable, IRegistry {
+
+    using EnumerableSet for EnumerableSet.AddressSet;
     
     address _factory;
+
+    // Map of refunders and their version, starting with 1
+    mapping(address => uint8) refunderVersion;
+
+    // Tuple of target address + identifier corresponding to set of refunders
+    mapping(address => mapping(bytes4 => EnumerableSet.AddressSet)) aggregatedRefundables;
+
+    // Set of refunders
+    EnumerableSet.AddressSet private refunders;
 
     modifier hasFactory {
         require(_factory != address(0), "Factory is not set");
@@ -20,5 +32,18 @@ contract Registry is Ownable, IRegistry {
 
     function updateRefunder(address refunder, bool active) external override hasFactory {
         require(msg.sender == _factory, "Caller is not the Factory");
+
+        refunders._add();
     }
+
+    // Returns all refunders
+    // function getRefunders() returns address[]
+
+    // Returns all refunders willing to sponsor the following target + identifier
+    // function refundersFor(address target, bytes4 identifier) returns address[]
+
+    // Only refunder contract can call. Adds the refunder contract in the Address Set
+    // If support is true -> refunder is marked to refund target+identifier calls
+    // If support is false -> refunder is marked NOT to refund target+identifier calls
+    // function updateRefundable(address target, bytes4 identifier, bool support)
 } 
